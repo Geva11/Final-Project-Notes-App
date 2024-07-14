1331c86840e6939c318c74cc8ac84c5b816bc6ec
Geva11 <165443898+Geva11@users.noreply.github.com>
2024-07-14 16:17:23 +0800

    Add files via upload

---
+++ ViewAllNotes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_Notes_App
+{
+    internal class ViewAllNotes
+    {
+        public static void UsersAllNotes(List<User> users)
+        {
+            if (users.Count == 0)
+            {
+                
+                Console.WriteLine("No users registered yet.\n");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("All User(s)'s Notes:");
+                int noteNumber = 1;
+                foreach (User user in users)
+                {
+                    Console.WriteLine($"User: {user.Email}");
+                    foreach (Note note in user.Notes)
+                    {
+                        Console.WriteLine(noteNumber + ". " + note.Title);
+                    }
+                    
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
+        }
+    }
+}
